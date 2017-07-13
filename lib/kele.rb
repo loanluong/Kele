d009bb9f30caea95fa329b01042e8c4792939261
lani luong <loanlu@gmail.com>
2017-07-13 12:29:51 -0700

    checkpoint submission

--- lib/kele.rb
+++ lib/kele.rb
@@ -32,4 +32,8 @@ class Kele
   def create_messages(sender, recipient_id, subject, stripped_text)
     response = self.class.post('https://www.bloc.io/api/v1/messages', body: { sender: sender, recipient_id: recipient_id, subject: subject, stripped_text: stripped_text }, headers: { "authorization" => @auth_token })
   end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    response = self.class.post('https://www.bloc.io/api/v1/checkpoint_submissions', body: { checkpoint_id: checkpoint_id, assignment_branch: assignment_branch, assignment_commit_link: assignment_commit_link, comment: comment, enrollment_id: enrollment_id }, headers: { "authorization" => @auth_token })
+  end
 end
